5c8d79760cc31180db7641613f516a07eb052542
yuki7iphone8-ui <yuki7iphone8@gmail.com>
2026-02-19 16:50:49 +0900

    Update Arduino_NANO_LED_BUILTIN.ino

--- Arduino_NANO_LED_BUILTIN.ino
+++ Arduino_NANO_LED_BUILTIN.ino
@@ -7,7 +7,8 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(300);                       // wait for a second
+  delay(500);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(300);                       // wait for a second
 }
+
