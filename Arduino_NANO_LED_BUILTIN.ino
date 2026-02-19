09d9461c8f0e40c8840e6623f7b068a286575853
yuki7iphone8-ui <yuki7iphone8@gmail.com>
2026-02-19 16:02:55 +0900

    Add files via upload

---
+++ Arduino_NANO_LED_BUILTIN.ino
@@ -0,0 +1,13 @@
+// Arduino NANO LED_BUILTIN
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);                       // wait for a second
+}
